b8dbfbdcf42942b050b3e1a876fea9708248affb
Artem Misanov <mesa4elo@gmail.com>
2023-01-30 12:17:28 +0200

    fix template + add cdn for remote components

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,25 +10,31 @@ const Home: NextPage = () => {
 			<SDKHead />
 			<div className="main">
 				<div className="container">
-					<div className="grid">
-						<div className="card">
+					<div className="card">
+						<div className="card__image">
 							<Remote id="2" />
-							<div className="description">
-								<Remote id="1" />
+						</div>
+						<div className="card__title">
+							<Remote id="1" />
+						</div>
+						<div className="card__sub-title">
+							<Remote id="3" />
+						</div>
+						<div className="card__gallery">
+							<div className="card__gallery-list">
+								<div className="card__gallery-list-item">
+									<Remote id="4" />
+								</div>
+								<div className="card__gallery-list-item">
+									<Remote id="5" />
+								</div>
 							</div>
 						</div>
-						<div className="card">
-							<Remote id="4" />
-							<div className="description">
-								<Remote id="3" />
-							</div>
-						</div>
-						<div className="card">
-							<Remote id="5" />
-							<div className="description">
-								<Remote id="6" />
-								<Remote id="7" />
-							</div>
+						<div className="card__description">
+							<Remote id="6" />
+							<br/>
+							<br/>
+							<Remote id="7" />
 						</div>
 					</div>
 				</div>
